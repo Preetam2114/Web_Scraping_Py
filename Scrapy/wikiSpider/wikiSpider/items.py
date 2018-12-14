f82befca443f1f6774a3f59ddc86b86443cf01db
Preetam2114 <preetamrane2114@gmail.com>
2018-12-14 22:43:40 +0530

    scrapy

--- Scrapy/wikiSpider/wikiSpider/items.py
+++ Scrapy/wikiSpider/wikiSpider/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class WikispiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Article(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    text = scrapy.Field()
+    lastUpdated = scrapy.Field()
+    
